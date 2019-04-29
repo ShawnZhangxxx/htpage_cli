13bf92f7efa915501e9ee0953f825de85f9c8158
ShawnZhangxxx <40831837+ShawnZhangxxx@users.noreply.github.com>
2019-04-29 16:42:58 +0800

    4/29 门户

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { initAllModule } = require("./lib/initModule/initModule");
 const { createComponent } = require("./lib/createComponent/createComponent");
 const { createList } = require("./lib/createListPage/createListPage");
 const { createQuery } = require("./lib/createQuery/createQuery");
+const { createPor } = require("./lib/createPor/createPor");
 
 console.log(process.argv)
 let command = process.argv[2];    
@@ -51,13 +52,32 @@ switch (command) {
             resPromise = createList(moduleName, componentName);
         }
         break;
+    case "-sql":
+    if (typeof moduleName !== "string") {
+            console.log("请输入正确的模块名称");
+        } else if (typeof componentName !== "string") {
+            console.log("请输入正确的弹出层名称");
+        } else {
+            resPromise = createSql(moduleName, componentName);
+        }
+        break;
+    case "-p":
+     if (typeof moduleName !== "string") {
+                console.log("请输入正确的模块名称");
+            } else if (typeof componentName !== "string") {
+                console.log("请输入正确的弹出层名称");
+            } else {
+                resPromise = createPor(moduleName, componentName);
+            }
+            break;    
     default:
         console.log("请输入正确的命令：\r\n" +
             "-m 参数：moduleName；功能：创建模块\r\n" +
             "-q 参数：moduleName queryName；功能：创建高级搜索组件\r\n" +
             // "-b 参数：moduleName；功能：创建后台文件\n" +
             "-c 参数：moduleName componentName；功能：创建弹出层\r\n" +
-            "-l 参数：moduleName pageName；功能：创建列表\r\n");
+            "-l 参数：moduleName pageName；功能：创建列表\r\n"+
+            "-p 参数：moduleName pageName；功能：创建门户列表\r\n");
 };
 if (resPromise) {
     resPromise.then(() => {
